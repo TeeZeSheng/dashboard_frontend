19d4af7d30d9c6c2fb70df02c31a7632ce80b185
TeeZeSheng <jasontzs007@gmail.com>
2024-10-11 14:15:32 +0800

    first commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
 
-    baseURL: 'https://dashboard-backend-e93b.onrender.com/v1/api',
+    baseURL: 'https://dashboard-backend-e93b.onrender.com/api/v1',
     timeout: 100000000,
     
   });
